23a52235b801bbdeb5817e45c31bccc2f386d648
AlcInt <148353020+A1cInt@users.noreply.github.com>
2026-07-26 01:32:06 +0300

    Fix device path case sensitivity in mk file
    Signed-off-by: AlcInt <148353020+A1cInt@users.noreply.github.com>

--- twrp_plato.mk
+++ twrp_plato.mk
@@ -33,7 +33,7 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 
 # Device specific configs
-$(call inherit-product, device/Xiaomi/plato/device.mk)
+$(call inherit-product, device/xiaomi/plato/device.mk)
 
 # Configure virtual_ab_ota.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
