21384556a8e16920455a69d1b5b119c6f6c4a89c
A1cInt <nikita.alex178@gmail.com>
2026-07-26 00:19:26 +0300

    I'm crazy looool

--- device.mk
+++ device.mk
@@ -131,7 +131,7 @@ PRODUCT_PACKAGES += \
 
 # Otacert
 PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/miui_releasekey
+    $(DEVICE_PATH)/security/releasekey
 
 # Hidl Service
 PRODUCT_ENFORCE_VINTF_MANIFEST := true
